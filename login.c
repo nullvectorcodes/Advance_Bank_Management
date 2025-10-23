9e499dcace909ddee5e70da80a68686a7a938d21
Mohammad Saalim <mohammadsaalim4718@gmail.com>
2025-10-23 06:46:02 +0530

    Changed the card validation logic

--- login.c
+++ login.c
@@ -9,20 +9,18 @@ int main(){
     char acc_num[20] = {0};
     printf("Enter Account Number: ");
     scanf("%19s",acc_num);
+    
     if (valid){
-        printf("\nThe card is Valid!\n");
-    }
-    else{
-        printf("\nThe Card Isn't Vlid!\n");
-    }
-    printf("Enter PIN: ");
-
-    while ((ch = _getch()) != 13){
+        printf("Enter PIN: ");
+        while ((ch = _getch()) != 13){
         pin[i] = ch;
         printf("*");
         i++;
     }
-
+    }
+    else{
+        printf("\nThe Card Isn't Vlid!\n");
+    }
 
     return 0;
 }

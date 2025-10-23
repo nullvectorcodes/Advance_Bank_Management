aade909fe8875d76816ee91e8ca4cda4e7f0de13
Mohammad Saalim <mohammadsaalim4718@gmail.com>
2025-10-23 06:44:06 +0530

    Added a pin hider

--- login.c
+++ login.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include "luhn_validator.h"
+#include <conio.h>
 
 int main(){
+    int ch , i =0;
+    char pin[6] = {0};
     int valid = 0;
     char acc_num[20] = {0};
-    int pin = 0;
     printf("Enter Account Number: ");
     scanf("%19s",acc_num);
-    printf("Enter PIN: ");
-    scanf("%d",&pin);
-
     if (valid){
-        printf("It's Valid!\n");
+        printf("\nThe card is Valid!\n");
     }
     else{
-        printf("Itsn't Valid!\n");
+        printf("\nThe Card Isn't Vlid!\n");
     }
+    printf("Enter PIN: ");
+
+    while ((ch = _getch()) != 13){
+        pin[i] = ch;
+        printf("*");
+        i++;
+    }
+
 
     return 0;
 }
